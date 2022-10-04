9a809af7ef562755a74afce1c28db6169cca09ae
MD Shahed <m.a.shahed@rocketdocs.com>
2022-10-04 01:03:44 -0400

    report FW

---
+++ test/example/exampleSimple.js
@@ -0,0 +1,13 @@
+"use strict";
+const assert = require("assert");
+describe("Example Test Top Describe", function () {
+  it("Example Test  Passing Test @pass", function () {
+    assert.equal(1, 1);
+  });
+  it("Example Test  Failing Test @fail", function () {
+    assert.equal(2, 1);
+  });
+  it.skip("Example Test Skipped Test @skip", function () {
+    assert.equal(2, 1);
+  });
+});
